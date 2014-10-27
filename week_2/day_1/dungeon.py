17a88f8e560e5b0a120f4eaf0a3b59f9810c7236
SlaviSotirov <slavi.sotirov@gmail.com>
2014-10-27 16:46:54 +0200

    Fixed dungeon.py

--- week_2/day_1/dungeon.py
+++ week_2/day_1/dungeon.py
@@ -1,3 +1,10 @@
 class Dungeon:
 
-    
+    def __init__(self, filename):
+        self.filename = filename
+
+    def _open_map(filename):
+        # map_file = open(filename, "r")
+        # temp = map_file.read().split("\n")
+        # for char in temp
+        # map_sym = []
